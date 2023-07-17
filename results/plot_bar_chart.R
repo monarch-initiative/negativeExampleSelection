f4c324320684dc0012605fe5a706c3af57d9d03c
Peter Robinson <peter.robinson@jax.org>
2023-07-17 10:24:04 -0400

    updating plot

--- results/plot_bar_chart.R
+++ results/plot_bar_chart.R
@@ -11,25 +11,11 @@ df <- read.csv(string_file_path)
 
 p_string <- ggplot(df,aes(x=methods, y=MCC.mean, fill=approach)) +
   geom_bar(stat="identity", position = "dodge", alpha=0.9) +
-  scale_x_discrete(limits = positions) +
-  ylim(0,1.1)+
-  coord_flip() +
-  scale_fill_jco() +
-  theme_bw() +
-  theme(
-    legend.title = element_blank(),
-    axis.title.x = element_blank(),
-    axis.text.y = element_text(size=14),
-    axis.text.x = element_text(size=18)) +
-  xlab("")
-ggsave( "string_eval.pdf",p_string)
-
-
-sli_file_path = "sli_stats.csv"
-df <- read.csv(sli_file_path)
-
-p_sli <- ggplot(df,aes(x=methods, y=MCC.mean, fill=approach)) +
-  geom_bar(stat="identity", position = "dodge", alpha=0.9) +
+  geom_col(position = position_dodge()) +
+  geom_errorbar(
+    aes(ymin = MCC.mean - MCC.sem, ymax = MCC.mean + MCC.sem), 
+    position = position_dodge2(padding = 0.5)
+  ) +
   scale_x_discrete(limits = positions) +
   ylim(0,1)+
   coord_flip() +
@@ -41,4 +27,30 @@ p_sli <- ggplot(df,aes(x=methods, y=MCC.mean, fill=approach)) +
     axis.text.y = element_text(size=14),
     axis.text.x = element_text(size=18)) +
   xlab("")
+p_string
+ggsave( "string_eval.pdf",p_string)
+
+
+sli_file_path = "sli_stats.csv"
+df <- read.csv(sli_file_path)
+
+p_sli <- ggplot(df,aes(x=methods, y=MCC.mean, fill=approach)) +
+  geom_bar(stat="identity", position = "dodge", alpha=0.9) +
+  geom_col(position = position_dodge()) +
+  geom_errorbar(
+    aes(ymin = MCC.mean - MCC.sem, ymax = MCC.mean + MCC.sem), 
+    position = position_dodge2(padding = 0.5)
+  ) +
+  scale_x_discrete(limits = positions) +
+  ylim(0,1)+
+  coord_flip() +
+  scale_fill_jco() +
+  theme_bw() +
+  theme(
+    legend.title = element_blank(),
+    axis.title.x = element_blank(),
+    axis.text.y = element_text(size=14),
+    axis.text.x = element_text(size=18)) +
+  xlab("")
+p_sli
 ggsave( "sli_eval.pdf", p_sli)
