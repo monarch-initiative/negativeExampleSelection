c1e6a82489560bbd417f729af20ff908bc0c0b46
Peter Robinson <peter.robinson@jax.org>
2023-10-23 16:54:01 +0200

    improving IDG script

--- scripts/runIDG.py
+++ scripts/runIDG.py
@@ -1,5 +1,4 @@
 from grape.datasets.kghub import KGIDG
-from tqdm import tqdm
 import pandas as pd
 from grape.edge_prediction import edge_prediction_evaluation
 from grape.edge_prediction import PerceptronEdgePrediction
@@ -24,7 +23,7 @@ dense_main_component.replace_edge_type_name_from_edge_node_type_names_inplace(
 )
 
 # Set smoke test to True for testing:
-SMOKE_TEST = False
+SMOKE_TEST = True
 NUMBER_OF_HOLDOUTS = 10
 VALIDATION_UNBALANCE_RATES = (1.0, )
 TRAIN_SIZES = (0.75,)
@@ -35,32 +34,20 @@ subgraph = dense_main_component.filter_from_names(
 
 # Disable the smoke test when you need to run the real thing:
 
-SMOKE_TEST = False
+SMOKE_TEST = True
 NUMBER_OF_HOLDOUTS = 10
 VALIDATION_UNBALANCE_RATES = (1.0, )
 TRAIN_SIZES = (0.75,)
 
 results = []
 
-for train_size in tqdm(
-    TRAIN_SIZES,
-    desc="Training sizes",
-    leave=False
-):
-    for validation_use_scale_free in tqdm(
-        (True, False),
-        desc="Validation use scale free",
-        leave=False
-    ):
-        for ModelClass in tqdm(
-            [
+for train_size in TRAIN_SIZES:
+    for validation_use_scale_free in (True, False):
+        for ModelClass in  [
                 FirstOrderLINEEnsmallen, SecondOrderLINEEnsmallen,
                 DeepWalkCBOWEnsmallen, DeepWalkSkipGramEnsmallen,
                 WalkletsCBOWEnsmallen, WalkletsSkipGramEnsmallen,
-            ],
-            desc="Embedding",
-            leave=False
-        ):
+            ]:
             # If the embedding method involves edge sampling, we train a run
             # using the scale free and one using the uniform.
             if "use_scale_free_distribution" in ModelClass().parameters():
@@ -74,7 +61,7 @@ for train_size in tqdm(
                 ]
             else:
                 parameter_sets = [dict()]
-
+            print(f"train size: {train_size}; validation_use_scale_free: {validation_use_scale_free} ModelClass {ModelClass}")
             for parameter_set in parameter_sets:
                 results.append(edge_prediction_evaluation(
                     smoke_test=SMOKE_TEST,
@@ -104,45 +91,3 @@ for train_size in tqdm(
 
 results = pd.concat(results)
 results.to_csv("kg_idg_negative_select.tsv",sep="\t")
-
-
-
-
-
-###
-
-results = []
-
-fresults = []
-train_size = 0.75
-for validation_use_scale_free in tqdm(
-    (True, False),
-    desc="Validation use scale free",
-    leave=False
-    ):
-    results.append(edge_prediction_evaluation(
-        smoke_test=SMOKE_TEST,
-        holdouts_kwargs=dict(
-            train_size=train_size,
-            edge_types=["minority_edge"],
-        ),
-        evaluation_schema="Connected Monte Carlo",
-        graphs=dense_main_component,
-        models=[
-            PerceptronEdgePrediction(
-                edge_features=edge_feature,
-                number_of_epochs=1000,
-                number_of_edges_per_mini_batch=16,
-                learning_rate=0.001,
-                use_scale_free_distribution=False
-            ) for edge_feature in("Degree","AdamicAdar","JaccardCoefficient","ResourceAllocationIndex","PreferentialAttachment")
-        ],
-        #number_of_slurm_nodes=NUMBER_OF_HOLDOUTS,
-        enable_cache=True,
-        number_of_holdouts=NUMBER_OF_HOLDOUTS,
-        use_scale_free_distribution=validation_use_scale_free,
-        validation_unbalance_rates=VALIDATION_UNBALANCE_RATES,
-        subgraph_of_interest=subgraph,
-        use_subgraph_as_support=True
-    ))
-
